65e11bbf94518de9436100ada7fac356ded750ca
Evgeny Lapin <lev@openteam.ru>
2011-10-10 10:16:34 +0700

    пользуем подгрузку css через application.css, а не через compass include

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  layout :resolve_layout
+
+  protected
+  def resolve_layout
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
+
 end
