b8266e0724768323e68253315c0d4a3855445f28
Evgeny Lapin <lev@openteam.ru>
2011-10-11 17:24:11 +0700

    #8463. GUI: datepicker на написании ответа

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def show_attribute(object, field)
+    return unless object
     content = ''
     content << content_tag(:span, object.class.human_attribute_name(field), :class => 'label')
     content << content_tag(:span, (((l object.send(field)) if object.send(field).is_a?(Date)) || (object.send("human_#{field}") if object.respond_to?("human_#{field}")) || (t('not_specified') if object.send(field).blank?) || object.send(field) || t('not_specified')), :class => 'value')
