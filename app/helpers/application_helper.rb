c8e72725174a310867d094d178d85bde7b4d8a84
Evgeny Lapin <lev@openteam.ru>
2011-10-12 10:20:45 +0700

    #8466. Исправить "Разрешить опубликовать true"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,15 @@ module ApplicationHelper
     return unless object
     content = ''
     content << content_tag(:span, object.class.human_attribute_name(field), :class => 'label')
-    content << content_tag(:span, (((l object.send(field)) if object.send(field).is_a?(Date)) || (object.send("human_#{field}") if object.respond_to?("human_#{field}")) || (t('not_specified') if object.send(field).blank?) || object.send(field) || t('not_specified')), :class => 'value')
+    content << content_tag(:span,
+                           (((l object.send(field)) if object.send(field).is_a?(Date)) ||
+                           (t("show_attribute.yes") if object.send(field).is_a?(TrueClass)) ||
+                           (t("show_attribute.no") if object.send(field).is_a?(FalseClass)) ||
+                           (object.send("human_#{field}") if object.respond_to?("human_#{field}")) ||
+                           (t('not_specified') if object.send(field).blank?) ||
+                           object.send(field) ||
+                           t('not_specified')),
+                           :class => 'value')
     content_tag :div, raw(content), :class => "show_attribute #{field.to_s}"
   end
 end
