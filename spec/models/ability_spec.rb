067906fdb901b3955c2b1596bb10fabd28718fac
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-10-14 13:51:00 +0700

    коммит после досадного выключения компьютера :'(

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -8,6 +8,8 @@ describe Ability do
   it { ability.should be_able_to(:create, Fabricate(:reply, :appeal => reviewing_appeal)) }
   it { ability.should be_able_to(:update, Fabricate(:reply, :appeal => reviewing_appeal)) }
 
+  it { ability.should be_able_to(:destroy, fresh_appeal) }
+
   it { ability.should be_able_to(:close, Fabricate(:appeal)) }
   it { ability.should be_able_to(:restore, Fabricate(:appeal)) }
   it { ability.should be_able_to(:review, Fabricate(:appeal)) }
