b4970961d8659717761aff4371f54c98413fbd33
Dmitry Lihachev <lda@openteam.ru>
2011-12-16 11:16:34 +0700

    #8965. Обновить sso_client el_vfs_client

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -6,8 +6,4 @@ Settings.extract!(:test, :development, :production)
 Settings.define 'hoptoad.api_key',            :env_var => 'HOPTOAD_API_KEY'
 Settings.define 'hoptoad.host',               :env_var => 'HOPTOAD_HOST'
 
-Settings.define 'el_vfs.protocol',            :env_var => 'EL_VFS_PROTOCOL'
-Settings.define 'el_vfs.host',                :env_var => 'EL_VFS_HOST'
-Settings.define 'el_vfs.port',                :env_var => 'EL_VFS_POST'
-
 Settings.resolve!
