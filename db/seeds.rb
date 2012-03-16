89d3b26ee80688bbe6e9d0825d266d1b417a527a
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-03-16 08:42:28 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 User.find_or_initialize_by_uid('1').tap do | user |
   user.save(:validate => false)
-user.permissions.create! :context => Context.first, :role => :manager if user.permissions.empty?
+  user.permissions.create! :context => Context.first, :role => :manager if user.permissions.empty?
 end
 
 Section.find_or_initialize_by_context_id(94).tap do | s |
