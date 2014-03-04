6914ae622c562adf6e87c3c810ef2fce919bc31b
Evgeny Lapin <lev@openteam.ru>
2014-03-04 09:41:23 +0700

    security update rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     content = ''
     content << content_tag(:span, object.class.human_attribute_name(field), :class => 'label')
     content << content_tag(:span,
-                           (((l object.send(field)) if object.send(field).is_a?(Date)) ||
+                           (((l object.send(field)) if object.send(field).is_a?(Date) || object.send(field).is_a?(DateTime) ||object.send(field).is_a?(Time) ) ||
                            (t("show_attribute.yes") if object.send(field).is_a?(TrueClass)) ||
                            (t("show_attribute.no") if object.send(field).is_a?(FalseClass)) ||
                            (object.send("human_#{field}") if object.respond_to?("human_#{field}")) ||
